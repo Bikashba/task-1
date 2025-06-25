d5cdb7f52eef936b80f281f21d9f494a4a22067c
Bikashba <bikashbarikb2005@gmail.com>
2025-06-25 08:21:58 +0530

    Q1.py

--- Q1.py
+++ Q1.py
@@ -1,1 +1,12 @@
+# 1.Which if does the else belong to? Explain why ?
+
+if True:  
+if False :
+    print("A")
+else:
+    print("B")
+
+#  First if true begins a block
+#  Inside it a inner if false has an else with same indentation level 
+# So the else is clearly paired with if false
  
